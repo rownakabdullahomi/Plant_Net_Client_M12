01dccc59ca1b95f937a76b640adbd32fe33d01a3
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-10 17:55:01 +0600

    plant post done

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,68 @@
-import { Helmet } from 'react-helmet-async'
-import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUpload } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [uploadButtonText, setUploadButtonText] = useState({
+    name: "Upload Image",
+  });
+  const [loading, setLoading] = useState(false);
+
+  // handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // before fetch loading true
+    setLoading(true);
+
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const image_url = await imageUpload(image);
+
+    // seller info
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    // create pant data object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: image_url,
+      seller,
+    };
+
+    console.table(plantData);
+
+    // save plant in db
+    try {
+      // post request
+      await axiosSecure.post("/plants", plantData)
+      toast.success("Data added successfully !");
+      
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       <Helmet>
@@ -9,9 +70,14 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadButtonText={uploadButtonText}
+        setUploadButtonText={setUploadButtonText}
+        loading={loading}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddPlant
+export default AddPlant;
